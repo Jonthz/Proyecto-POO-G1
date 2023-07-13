95b9207bc402ae861c3e073d457afc32d1f5f074
scarCevallos <scayceva@espol.edu.ec>
2023-07-12 23:44:12 -0500

    Se agregaron atributos y constructor de vehiculo

--- VentaDeVehiculos/src/main/java/Vehiculos_Package/Vehiculo.java
+++ VentaDeVehiculos/src/main/java/Vehiculos_Package/Vehiculo.java
@@ -4,10 +4,108 @@
  */
 package Vehiculos_Package;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Scarlet Cevallos
  */
 public class Vehiculo {
+    protected String placa;
+    protected String marca;
+    protected String modelo;
+    protected String tipoMotor;
+    protected String anio;
+    protected String recorrido;
+    protected String color;
+    protected String tipoCombustible;
+    protected double precio;
+    protected ArrayList<Oferta>ofertas;
+    
+    public Vehiculo(String placa, String marca, String modelo, String tipoMotor, String anio, String recorrido, String color, String tipoCombustible, double precio) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipoMotor = tipoMotor;
+        this.anio = anio;
+        this.recorrido = recorrido;
+        this.color = color;
+        this.tipoCombustible = tipoCombustible;
+        this.precio = precio;
+        this.ofertas = new ArrayList<>();
+    } 
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getTipoMotor() {
+        return tipoMotor;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public String getRecorrido() {
+        return recorrido;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setTipoMotor(String tipoMotor) {
+        this.tipoMotor = tipoMotor;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public void setRecorrido(String recorrido) {
+        this.recorrido = recorrido;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
     
 }
