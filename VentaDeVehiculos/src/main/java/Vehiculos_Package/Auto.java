dea5f0468d7daa3ea3c6c0c757a00f5268493b58
scarCevallos <scayceva@espol.edu.ec>
2023-07-12 23:37:59 -0500

    Se agregaron atributos de auto

--- VentaDeVehiculos/src/main/java/Vehiculos_Package/Auto.java
+++ VentaDeVehiculos/src/main/java/Vehiculos_Package/Auto.java
@@ -9,5 +9,12 @@ package Vehiculos_Package;
  * @author Scarlet Cevallos
  */
 public class Auto extends Vehiculo{
-    
+    private String vidrios;
+    private String transmision;
+
+    public Auto(String vidrios, String transmision, String placa, String marca, String modelo, String tipoMotor, String anio, String recorrido, String color, String tipoCombustible, double precio) {
+        super(placa, marca, modelo, tipoMotor, anio, recorrido, color, tipoCombustible, precio);
+        this.vidrios = vidrios;
+        this.transmision = transmision;
+    } 
 }
