83ef9fc78cbb74e38b80c6d4980e35daeddf08be
Jonthz <jnzambra@fiec.espol.edu.ec>
2023-07-16 19:46:29 -0500

    stash

--- VentaDeVehiculos/src/main/java/Vehiculos_Package/Vendedor.java
+++ VentaDeVehiculos/src/main/java/Vehiculos_Package/Vendedor.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  * @author HP
  */
 public class Vendedor extends Usuario {
+
     private ArrayList<Vehiculo> vehiculos;
 
     public Vendedor(int id, String n, String ap, String org, String correo, String clave) {
@@ -234,4 +235,5 @@ public class Vendedor extends Usuario {
         }
         return null;
     }
+
 }
