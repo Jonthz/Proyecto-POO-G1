a251909e828725c19afd0bf26bb160d8e51808fe
Jonthz <jnzambra@fiec.espol.edu.ec>
2023-07-18 23:28:43 -0500

    javax mail

--- VentaDeVehiculos/src/main/java/ec/edu/espol/ventadevehiculos/VentaDeVehiculos.java
+++ VentaDeVehiculos/src/main/java/ec/edu/espol/ventadevehiculos/VentaDeVehiculos.java
@@ -6,7 +6,6 @@ package ec.edu.espol.ventadevehiculos;
 
 import Vehiculos_Package.*;
 import Vehiculos_Package.Utilitaria;
-import ec.edu.espol.ventadevehiculos.clases.Usuario;
 import java.util.ArrayList;
 
 
@@ -19,9 +18,10 @@ public class VentaDeVehiculos {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //Utilitaria.vaciarArchivo("compradores.txt");
-        
-        Utilitaria u = new Utilitaria();
-        u.menu_principal();
+//        
+//        Utilitaria u = new Utilitaria();
+//        u.menu_principal();
+Utilitaria.enviarConGMail("cuentafantasma63@gmail.com", "¿Es confuso, verdad?", " Sin embargo sabes perfectamente cuando estás mal, todo tu cuerpo física y mentalmente te lo hace saber te notas flojo con pensamiento fatalistas, esa sensación que está todo perdido de que ya nada volverá a ser como antes te torturas recordado una vivencia pasada aleatoria que en aquel entonces ni siquiera parecía un buen momento pero comparado como te sientes ahora podría incluso decirse que. ¿Fuiste feliz sin saberlo?");
 
     }
     
